0c17ac61b6fdd5c02035f80cd49997d882b653f6
I551381 <cedrik.baedorf@sap.com>
2023-10-16 16:53:27 +0200

    2

--- src/main/java/smart/housing/database/Main.java
+++ src/main/java/smart/housing/database/Main.java
@@ -1,11 +1,8 @@
 package smart.housing.database;
 
 import smart.housing.entities.User;
-import smart.housing.security.SimpleHashAlgorithm;
 
 import javax.persistence.EntityManager;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
 
