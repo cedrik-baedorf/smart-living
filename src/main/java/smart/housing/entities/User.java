e1827ea9d06f88bf2272c8c44b5c0f1cb78a435c
I551381 <cedrik.baedorf@sap.com>
2023-10-16 10:59:35 +0200

    update

--- src/main/java/smart/housing/entities/User.java
+++ src/main/java/smart/housing/entities/User.java
@@ -58,7 +58,7 @@ public class User {
         this.lastName = lastName;
     }
 
-    public void setFirstName(String fistName) {
+    public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
