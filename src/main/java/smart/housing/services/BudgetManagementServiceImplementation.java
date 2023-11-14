b967b648546ae65128e666293eb803f8014ca2c4
ivanwasnotavailable <ivan@luzie.de>
2023-11-14 12:12:13 +0100

    Ivan macht Sachen

--- src/main/java/smart/housing/services/BudgetManagementServiceImplementation.java
+++ src/main/java/smart/housing/services/BudgetManagementServiceImplementation.java
@@ -2,6 +2,7 @@ package smart.housing.services;
 
 import smart.housing.database.DatabaseConnector;
 import smart.housing.entities.User;
+import smart.housing.entities.Task;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
@@ -9,16 +10,24 @@ import java.util.List;
 
 public class BudgetManagementServiceImplementation implements BudgetManagementService{
 
-    private DatabaseConnector databaseConnector;
+    private final DatabaseConnector DATABASE_CONNECTOR;
 
     public BudgetManagementServiceImplementation(DatabaseConnector databaseConnector) {
-        this.databaseConnector = databaseConnector;
+        this.DATABASE_CONNECTOR = databaseConnector;
     }
 
     @Override
     public List<User> getCurrentUsers() {
-        EntityManager entityManager = databaseConnector.createEntityManager();
+        EntityManager entityManager = DATABASE_CONNECTOR.createEntityManager();
         TypedQuery<User> typedQuery = entityManager.createNamedQuery(User.FIND_ALL, User.class);
         return typedQuery.getResultList();
+    }
+
+    @Override
+    public List<Task> getAllExpenses(){
+        EntityManager entityManager = DATABASE_CONNECTOR.createEntityManager();
+        List<Task> taskList = entityManager.createNamedQuery(Task.FIND_ALL, Task.class).getResultList();
+        entityManager.close();
+        return taskList;
     }
 }
