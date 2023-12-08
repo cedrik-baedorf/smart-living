a9f3d50724cc34794cbb3a632beb6ebd65a47fc3
I551381 <cedrik.baedorf@sap.com>
2023-12-08 08:48:31 +0100

    user dialog done

--- src/main/java/smart/housing/ui/UserDataDialog.java
+++ src/main/java/smart/housing/ui/UserDataDialog.java
@@ -1,16 +1,21 @@
 package smart.housing.ui;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
+import javafx.event.ActionEvent;
 import javafx.scene.control.Dialog;
 import smart.housing.entities.User;
 import smart.housing.enums.UserRole;
+import smart.housing.exceptions.EmptyFieldException;
+import smart.housing.exceptions.UserManagementServiceException;
+import smart.housing.security.HashAlgorithm;
 
 public class UserDataDialog extends Dialog<User> {
+
+    private User applicationUser, user;
 
     private StyledTextField username, firstName, lastName, email;
     private StyledPasswordField password, confirmPassword;
     private StyledComboBox<UserRole> role;
+    private StyledButton confirmButton;
 
     /**
      * This constructor takes two arguments of class {@link User}
@@ -18,7 +23,16 @@ public class UserDataDialog extends Dialog<User> {
      * @param user user object who's properties will be loaded into the input fields
      */
     public UserDataDialog(User applicationUser, User user) {
+        this.applicationUser = applicationUser;
+        this.user = user;
+
         this.createFields();
+
+        clearFields();
+        if(user != null)
+            this.loadUserData();
+
+        confirmButton.setOnAction(this::createUser);
     }
 
     private void createFields() {
@@ -41,6 +55,61 @@ public class UserDataDialog extends Dialog<User> {
 
         confirmPassword = new StyledPasswordField();
         username.setPromptText("confirm password");
+
+        confirmButton = new StyledButton();
+    }
+
+    private void clearFields() {
+        this.username.clear();
+        this.firstName.clear();
+        this.lastName.clear();
+        this.role.setValue(UserRole.DEFAULT_ROLE);
+        this.email.clear();
+    }
+
+    private void loadUserData() {
+        this.username.setText(user.getUsername());
+        this.firstName.setText(user.getFirstName());
+        this.lastName.setText(user.getLastName());
+        this.role.setValue(user.getRole());
+        this.email.setText(user.getEmail());
+
+    }
+
+    private void createUser(ActionEvent event) {
+        event.consume();
+        try {
+            this.setResult(createUser());
+        } catch (EmptyFieldException exception) {
+            new ErrorDialog(exception.getMessage());
+        } catch (UserManagementServiceException exception) {
+            new ErrorDialog(exception.getMessage()).show();
+        } finally {
+            clearFields();
+            if(this.user != null)
+                this.loadUserData();
+        }
+    }
+
+    private User createUser() {
+        if(! password.getText().equals(confirmPassword.getText()))
+            throw new UserManagementServiceException("Passwords do not match");
+
+        User user = new User(username.getText());
+
+        if(password.getText().isEmpty()) {
+            if(this.user == null)
+                throw new EmptyFieldException("Please enter a password", "password");
+            else
+                user.setPasswordHash(this.user.getPassword());
+        } else
+            user.setPassword(password.getText(), HashAlgorithm.DEFAULT);
+
+        user.setFirstName(firstName.getText());
+        user.setLastName(lastName.getText());
+        user.setRole(role.getValue());
+        user.setEmail(email.getText());
+        return user;
     }
 
 }
